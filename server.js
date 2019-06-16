fce481ed039ce7beede68e561bb6b912bc27628a
Reuben Osborne <reuben.osborne@gmail.com>
2019-06-16 19:39:53 +1200

    Made local repo

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const hbs = require('express-handlebars')
 
 const userRoutes = require('./routes/users')
 
-const server = express()
+const app = express()
 
 
 
 // Middleware
 
-server.engine('hbs', hbs({ extname: 'hbs' }))
-server.set('view engine', 'hbs')
-server.use(express.urlencoded({ extended: true }))
+app.engine('hbs', hbs({ extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
 // Routes
 
-server.use('/', userRoutes)
-server.use(express.static('public'))
+app.use('/', userRoutes)
+app.use(express.static(__dirname + '/public'));
 
-module.exports = server
+module.exports = app
